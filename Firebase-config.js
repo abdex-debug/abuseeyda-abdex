28dd52e98f4f398a6af08d80e53236c2845ac4de
abdex-debug <abdetareshid23@gmail.com>
2026-09-17 02:45:20 +0300

    Add Firebase configuration file

---
+++ Firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBip33kIPaNUOYK2c1AUxHfYHPJRL6oRw0",
+  authDomain: "abuseeyda.firebaseapp.com",
+  projectId: "abuseeyda",
+  storageBucket: "abuseeyda.firebasestorage.app",
+  messagingSenderId: "617922808847",
+  appId: "1:617922808847:web:b1504ac8d884f6da05a77a",
+  measurementId: "G-MX4FRNQTV0"
+};
+
+export { firebaseConfig };
